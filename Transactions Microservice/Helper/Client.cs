c918813f8692ba9b08cb8c446adb4f635c7397fe
Yogesh <sharmayogesh.ca00@gmail.com>
2020-11-12 17:05:51 +0530

    First Commit

---
+++ Transactions Microservice/Helper/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Transactions_Microservice.Helper
+{
+    public class Client
+    {
+        public HttpClient AccountDetails()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://52.224.198.245");
+            return client;
+        }
+
+        public HttpClient RuleApi()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://40.76.163.146");
+            return client;
+        }
+    }
+}
